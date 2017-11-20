d61f2dbeca19cf483a092b99db339303be069408
Jon C <jchan178@gmail.com>
2017-11-19 21:11:50 -0600

    crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,59 @@
-var numbToGuess = Math.floor((Math.random() * 100) + 19);
-
-$("#randomNumber").text(numbToGuess);
-
+var numbToGuess;
 var wins = 0;
 var losses = 0;
+var yourNumber = 0
 
 $("#wins").text("Wins: " + wins);
 $("#losses").text("Losses: " + losses);
 
-var yourNumber = 0
+var randNumb = function() {
+  numberToGuess = Math.floor(Math.random() * (120-19) + 19);
+  $("#randomNumber").text(numberToGuess);
+}
+randNumb();
 
-$("#yrNumb").text(yourNumber)
+var game = function () {
+  $(".bevo").attr("data-crystalvalue", Math.floor(Math.random() * (12-1) + 1));
+  $(".longhorn").attr("data-crystalvalue", Math.floor(Math.random() * (12-1) + 1));
+  $(".stadium").attr("data-crystalvalue", Math.floor(Math.random() * (12-1) + 1));
+  $(".tower").attr("data-crystalvalue", Math.floor(Math.random() * (12-1) + 1));
+}
+game();
+
+$("#yrNumb").text(yourNumber);
 
 $(".crystal").on("click", function() {
-	yourNumber++;
-	console.log(yourNumber);
+  var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+  yourNumber += crystalValue;
+
+  console.log(yourNumber);
+
+  if (yourNumber === numberToGuess) {
+	$("#yrNumb").text(yourNumber);
+	alert("You win");
+	game();
+	randNumb();
+	yourNumber = 0;
 	$("#yrNumb").text(yourNumber);
 
-  if (yourNumber === numbToGuess) {
+  } else {
+  	$("#yrNumb").text(yourNumber);
+  }
+
+  if (yourNumber > numberToGuess) {
 	$("#yrNumb").text(yourNumber);
-	alert("you win")
+	alert("You lose");
+	game();
+	randNumb();
+	yourNumber = 0;
+	$("#yrNumb").text(yourNumber);
   }
 
 })
+
+
+
+
 
 
